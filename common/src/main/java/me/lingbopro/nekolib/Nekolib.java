14519fef0e02bfcfe4884b3f0db42f3646c78868
lingbopro <ilovemc@zohomail.cn>
2026-07-22 15:00:58 +0800

    Basic stuff

--- common/src/main/java/me/lingbopro/nekolib/Nekolib.java
+++ common/src/main/java/me/lingbopro/nekolib/Nekolib.java
@@ -1,9 +1,15 @@
 package me.lingbopro.nekolib;
 
+import com.mojang.logging.LogUtils;
+import dev.architectury.platform.Platform;
+import org.slf4j.Logger;
+
 public final class Nekolib {
     public static final String MOD_ID = "nekolib";
 
+    public static final Logger LOGGER = LogUtils.getLogger();
+
     public static void init() {
-        // Write common init code here.
+        LOGGER.info("Hello world! NekoLib initializing on {}!", Platform.getEnvironment());
     }
 }
