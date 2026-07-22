ad0c46b0e384095b384ee8aed716145d63ef31f2
lingbopro <ilovemc@zohomail.cn>
2026-07-22 14:42:24 +0800

    Init from (partial modified) template

---
+++ common/src/main/java/me/lingbopro/nekolib/Nekolib.java
@@ -0,0 +1,9 @@
+package me.lingbopro.nekolib;
+
+public final class Nekolib {
+    public static final String MOD_ID = "nekolib";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/me/lingbopro/nekolib/neoforge/NekolibNeoForge.java
@@ -0,0 +1,12 @@
+package me.lingbopro.nekolib.neoforge;
+
+import me.lingbopro.nekolib.Nekolib;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Nekolib.MOD_ID)
+public final class NekolibNeoForge {
+    public NekolibNeoForge() {
+        // Run our common setup.
+        Nekolib.init();
+    }
+}
